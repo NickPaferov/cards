f70f2b24d7510775a95b153e0816d554eaed7e2a
Evgeniy Chernyshenko <yevgeniyche@gmail.com>
2022-10-17 02:07:23 +0300

    feat: implement sign up page

--- src/layouts/BaseLayout/BaseLayout.tsx
+++ src/layouts/BaseLayout/BaseLayout.tsx
@@ -1,22 +1,49 @@
 import { Header } from "./Header/Header";
 import s from "./BaseLayout.module.css";
 import { Main } from "./Main/Main";
-import { LinearProgress } from "@mui/material";
+import { IconButton, LinearProgress } from "@mui/material";
 import { useAppSelector } from "../../hooks/useAppSelector";
-
-type PropsType = {
-  center?: boolean;
-  wrap?: boolean;
-};
+import { useSnackbar } from "notistack";
+import { useEffect } from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
 export const BaseLayout = ({ center, wrap }: PropsType) => {
-  const isLoading = useAppSelector((state) => state.app.isLoading);
+  const { isLoading, snackbar } = useAppSelector((state) => state.app);
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+
+  useEffect(() => {
+    snackbar.message &&
+      enqueueSnackbar(snackbar.message, {
+        variant: snackbar.variant,
+        action: (key) => (
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={() => {
+              closeSnackbar(key);
+            }}
+          >
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        ),
+      });
+  }, [snackbar, enqueueSnackbar, closeSnackbar]);
 
   return (
     <div className={s.wrapper}>
-      {isLoading && <LinearProgress />}
+      {isLoading && (
+        <LinearProgress
+          sx={{ position: "absolute", width: "100%", zIndex: 2 }}
+        />
+      )}
       <Header />
       <Main center={center} wrap={wrap} />
     </div>
   );
+};
+
+type PropsType = {
+  center?: boolean;
+  wrap?: boolean;
 };
---
+++ src/pages/SignUpPage/CheckEmailScreen/CheckEmailScreen.tsx
@@ -0,0 +1,38 @@
+import { Button } from "@mui/material";
+import s from "./CheckEmailScreen.module.css";
+import letter from "../../../assets/images/letter.svg";
+import { Link, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+export const CheckEmailScreen = ({ email }: PropsType) => {
+  const [count, setCount] = useState(5);
+
+  useEffect(() => {
+    const counInterval = setInterval(() => {
+      setCount((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(counInterval);
+  }, []);
+
+  if (count === 0) {
+    return <Navigate to="/signin" />;
+  }
+
+  return (
+    <div className={s.wrapper}>
+      <h1>Sign up successfully</h1>
+      <img src={letter} alt="Letter" className={s.image} />
+      <span className={s.text}>
+        Your email {email}. You will be redirected to the sign in page
+      </span>
+      <Button variant="contained" component={Link} to="/signin">
+        Back to login after {count}
+      </Button>
+    </div>
+  );
+};
+
+type PropsType = {
+  email: string;
+};
