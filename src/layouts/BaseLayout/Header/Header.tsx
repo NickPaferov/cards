ae296b202cc860718cb55168912c889ce236f1b9
Paferov Nikolai <paferov.nikolai@gmail.com>
2022-10-18 16:47:56 +0300

    feat: implement sign in page

--- src/layouts/BaseLayout/Header/Header.tsx
+++ src/layouts/BaseLayout/Header/Header.tsx
@@ -4,8 +4,11 @@ import { Container } from "../Container/Container";
 import { Link } from "react-router-dom";
 import { UserItem } from "./UserItem/UserItem";
 import { Button } from "@mui/material";
+import { useAppSelector } from "../../../hooks/useAppSelector";
 
 export const Header = () => {
+  const user = useAppSelector((state) => state.auth.user);
+
   return (
     <nav className={s.wrapper}>
       <Container>
@@ -13,7 +16,7 @@ export const Header = () => {
           <Link to="">
             <img src={logo} alt="IT-INCUBATOR" className={s.logo} />
           </Link>
-          {true ? (
+          {!user ? (
             <Button variant="contained" component={Link} to="/signin">
               Sign in
             </Button>
