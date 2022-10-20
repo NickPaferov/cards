01f89652bdcb53b7264f2f706217a5d90a7552da
Pavel-Solovev <pavelsol1990@gmail.com>
2022-10-20 20:44:29 +0300

    fetch: create ForgotPasswordPage.tsx, ForgotPasswordPage.module.css, ResetPasswordPage.tsx, ResetPassword.module.css and POST request forgotPassword(mail, from, mess), resetPassword(pass, token)

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -5,6 +5,16 @@ export const authApi = {
     (await client.post("auth/register", data)).data,
   login: async (data: LoginParamsType) =>
     (await client.post<LoginResponseType>("auth/login", data)).data,
+  forgotPassword: async (data: ResetParamsType) =>
+    (
+      await client.post<ResetParamsResponseType>(
+        "https://neko-back.herokuapp.com/2.0/auth/forgot",
+        data
+      )
+    ).data,
+  resetPassword: async (data: ResetPasswordType) =>
+    (await client.post<ResetParamsResponseType>("/auth/set-new-password", data))
+      .data,
 };
 
 export type RegisterParamsType = {
@@ -31,4 +41,20 @@ export type LoginResponseType = {
   __v: number;
   token: string;
   tokenDeathTime: number;
+};
+
+export type ResetParamsType = {
+  email: string;
+  from: string;
+  message: string;
+};
+
+export type ResetParamsResponseType = {
+  info: string;
+  error: string;
+};
+
+export type ResetPasswordType = {
+  password: string;
+  resetPasswordToken: string;
 };
--- src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -1,3 +1,72 @@
+import { TextField } from "@mui/material";
+import s from "./ForgotPasswordPage.module.css";
+import { useForm, SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { authThunks } from "../../store/auth-reducer";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { LoadingButton } from "@material-ui/lab";
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
+
+const schema = yup.object({
+  email: yup.string().required("Email is required").email("Invalid email"),
+});
+
 export const ForgotPasswordPage = () => {
-  return <h1>Forgot Password</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<Inputs>({
+    resolver: yupResolver(schema),
+  });
+  const [isSuccessful, setIsSuccessful] = useState(false);
+  const dispatch = useAppDispatch();
+
+  const onSubmit: SubmitHandler<Inputs> = async ({ email }) => {
+    const from = "test-front-admin <ai73a@yandex.by>";
+    const message = `<div style=\"background-color: lime; padding: 15px\">password recovery link:<a href='http://localhost:3000/cards?#/reset-password/$token$'>link</a></div>`;
+
+    const isSuccessful = await dispatch(
+      authThunks.forgotPassword({ email, from, message })
+    );
+    setIsSuccessful(isSuccessful);
+  };
+
+  if (isSuccessful) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div className={s.wrapper}>
+      <h1>Forgot Password</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className={s.form}>
+        <div className={s.fieldsContainer}>
+          <TextField
+            label={errors.email ? errors.email.message : "Email"}
+            variant="standard"
+            {...register("email")}
+            error={!!errors.email}
+          />
+        </div>
+        <LoadingButton
+          type="submit"
+          className={s.button}
+          loading={isSubmitting}
+          loadingPosition="start"
+          variant="contained"
+          startIcon={<></>}
+        >
+          Forgot
+        </LoadingButton>
+      </form>
+    </div>
+  );
+};
+
+type Inputs = {
+  email: string;
+  from: string;
+  message: string;
 };
